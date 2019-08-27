cafaceeda4fc411aa24a055d060180769c0c56b7
zhengchengc <zhengchengc@student.unimelb.edu.au>
2019-08-27 23:22:15 +1000

    handling errors

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const request = require("request");
 
-const url = 'https://api.darksky.net/forecast/bb77ab25f7ec9102c1e6e6a8e24ee1c2/37.8267,-122.4233';
+const url = 'https://api.darksky.net/forecast/bb77ab25f7ec9102c1e6e6a8e24ee1c2/31.3100,118.9730?units=si';
 
 request({ url: url, json: true }, (err, res) => {
-    console.log(res.body.currently);
+    if (err) {
+        console.log('Unable to connect to weahter service!');
+    } else if (res.body.error) {
+        console.log('Unable to find location');
+    } else {
+        console.log(res.body.daily.data[0].summary);
+        console.log('It is currently ' + res.body.currently.temperature + ' degrees out.');
+        console.log('There is ' + res.body.currently.precipProbability + 'chance of rain.')
+    }
 });
 
 const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Melbourne.json?access_token=pk.eyJ1IjoiZ3pjIiwiYSI6ImNqenRzZGUwcDA4bjczbm55Z29zNWhmbmUifQ.cAsIl2zOQqqeDbmYhud8yg';
 
 request({ url: geocodeURL, json: true }, (err, res) => {
-    const latitude = res.body.features[0].center[0];
-    const longitude = res.body.features[0].center[1];
-    console.log(latitude, longitude);
+    if (err) {
+        console.log('Unable to connect to geocoding service');
+    } else if (res.body.features.length === 0) {
+        console.log('Unable to find location. Try another serach!')
+    } else {
+        const latitude = res.body.features[0].center[0];
+        const longitude = res.body.features[0].center[1];
+        console.log(latitude, longitude);
+    }
 });
